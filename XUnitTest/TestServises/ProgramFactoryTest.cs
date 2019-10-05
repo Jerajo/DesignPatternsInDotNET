d7055d5456e3115067a1f0e31aaa76b2d7a25561
Jesse R. Jose <jessejose@outlook.es>
2019-10-05 11:18:29 -0400

    Unit Test proyect added

---
+++ XUnitTest/TestServises/ProgramFactoryTest.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+using DesignPatterns.Services;
+using DesignPatterns.Modules;
+
+namespace XUnitTest.TestServises
+{
+	public class ProgramFactoryTest
+	{
+		ProgramFactory _programFactory;
+		public ProgramFactoryTest()
+		{
+			_programFactory = new ProgramFactory(new LoggerService());
+		}
+
+		[Fact]
+		public void CreateNotNullProgramTest()
+		{
+
+			var program = _programFactory.Create(nameof(AdapterProgram));
+
+			Assert.Equal(typeof(AdapterProgram), program.GetType());
+			Assert.NotEqual(typeof(NullProgram), program.GetType());
+		}
+
+		[Fact]
+		public void CreateNullProgramTest()
+		{
+			var program = _programFactory.Create("");
+
+			Assert.Equal(typeof(NullProgram), program.GetType());
+			Assert.NotEqual(typeof(AdapterProgram), program.GetType());
+		}
+	}
+}
